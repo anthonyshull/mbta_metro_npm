813aee14b7a82dfa3fd109c7a5ddb891464a2429
Anthony Shull <shull.anthony@gmail.com>
2024-09-13 10:47:38 -0500

    remove css and properly reference metro components

--- js/styles/content.js
+++ js/styles/content.js
@@ -1,6 +1,5 @@
 export default [
   "node_modules/preline/dist/*.js",
-  "../lib/mbta_metro/components/**/*.ex",
-  "../lib/mbta_metro/live/**/*.ex",
-  "../lib/mbta_metro_web/**/*.*ex"
+  "../deps/mbta_metro/components/**/*.ex",
+  "../deps/mbta_metro/live/**/*.ex"
 ]
