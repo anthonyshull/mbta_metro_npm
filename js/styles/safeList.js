fdb563da773caee9cb7e5257635f51ab647667bc
Anthony Shull <shull.anthony@gmail.com>
2024-09-16 18:29:00 -0500

    theme variants

--- js/styles/safeList.js
+++ js/styles/safeList.js
@@ -3,7 +3,7 @@ import { customColors } from "./colors"
 const colors = Object.keys(customColors);
 const backgrounds = colors.map(color => {
   return {
-    pattern: new RegExp(String.raw(`^bg-${color}-\d+`), "g"),
+    pattern: new RegExp(`^bg-${color}-`, "g"),
     variants: ["hover"]
   }
 });
