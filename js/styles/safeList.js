d2a299f2166302c5d6fc2d7d64c96f63b66942d3
Anthony Shull <shull.anthony@gmail.com>
2024-09-17 08:41:36 -0500

    regex

--- js/styles/safeList.js
+++ js/styles/safeList.js
@@ -3,7 +3,7 @@ import { customColors } from "./colors"
 const colors = Object.keys(customColors);
 const backgrounds = colors.map(color => {
   return {
-    pattern: new RegExp(`^bg-${color}-`, "g"),
+    pattern: new RegExp(`bg-${color}`, "g"),
     variants: ["hover"]
   }
 });
